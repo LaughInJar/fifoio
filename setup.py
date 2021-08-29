8b13b6d9d966fbfe8c3633139f00a83bd97814c1
Simon <simon@ciuvo.com>
2021-08-29 21:37:38 +0200

    at setup.py to build distribution

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import fifoio
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fifoio",
+    version=fifoio.__version__,
+    author=fifoio.__author__,
+    author_email=fifoio.__email__,
+    description=fifoio.__doc__,
+    long_description=fifoio.__doc__,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LaughInJar/fifoio",
+    project_urls={
+        "Bug Tracker": "https://github.com/LaughInJar/fifoio/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
