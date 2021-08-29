5ebcf2743fa40325252267304ea7c0ad608a4b4b
Simon <simon@ciuvo.com>
2021-08-29 22:24:32 +0200

    add tests for readinto()

--- tests/test_sharedbuffer.py
+++ tests/test_sharedbuffer.py
@@ -184,3 +184,69 @@ class WriteTestCase(TestCase):
         asyncio.run(main())
         self.assertEqual(waiting[0], "done")
 
+
+class ReadIntoTestCase(TestCase):
+    """Tests for :py:meth:`fifoio.SharedBuffer.read`"""
+
+    #: copy the tests from the write test case.. not nice
+    test_read_write = WriteTestCase.test_read_write
+
+    #: copy the tests from the write test case.. not nice
+    test_read_write_circular = WriteTestCase.test_read_write_circular
+
+    def test_block_on_empty(self):
+        """test that it blocks when the buffer is full"""
+        buffer = SharedBuffer(buffer_size=10)
+        self.assertEqual(buffer.available(), 0)
+        history = []
+
+        def read_blocked() -> None:
+            """write to full buffer"""
+            history.append("before read")
+            buffer.readinto(bytearray(3))
+            history.append("after read")
+
+        def write() -> None:
+            """read from buffer"""
+            history.append("before write")
+            buffer.write(b"abc")
+            history.append("write done")
+
+        async def main() -> None:
+            read_task = asyncio.create_task(run_in_thread(read_blocked))
+            asyncio.sleep(0.1)
+            write_task = asyncio.create_task(run_in_thread(write))
+            await asyncio.gather(read_task, write_task)
+
+        asyncio.run(main())
+        self.assertListEqual(
+            history,
+            [
+                "before read",
+                "before write",
+                "write done",
+                "after read",
+            ],
+        )
+
+    def test_notify_full(self):
+        """test that the empty condition is notified after a write"""
+        buffer = SharedBuffer(buffer_size=3)
+        buffer.write(b"abc")
+        waiting = []
+
+        def wait_for_notify(waiting: list) -> None:
+            """"""
+            with buffer._full:
+                if buffer._full.wait(timeout=1):
+                    waiting.append("done")
+                else:
+                    waiting.append("timeout")
+
+        async def main() -> None:
+            task1 = asyncio.create_task(run_in_thread(wait_for_notify, waiting))
+            task2 = asyncio.create_task(run_in_thread(buffer.readinto, bytearray(3)))
+            await asyncio.gather(task1, task2)
+
+        asyncio.run(main())
+        self.assertEqual(waiting[0], "done")
