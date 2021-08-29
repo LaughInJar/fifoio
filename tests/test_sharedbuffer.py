80fa4ceaf32b96dd7918713c5ef7864cfb47dc92
Simon <simon@ciuvo.com>
2021-08-29 22:11:59 +0200

    add tests for the write method

--- tests/test_sharedbuffer.py
+++ tests/test_sharedbuffer.py
@@ -10,8 +10,13 @@ import pytest
 from fifoio import SharedBuffer
 
 
+async def run_in_thread(func: Callable, *args) -> None:
+    loop = asyncio.get_running_loop()
+    await loop.run_in_executor(None, func, *args)
+
+
 class IndicateEofTestCase(TestCase):
-    """Tests for :py:class:`fifoio.SharedBuffer.indicate_eof`"""
+    """Tests for :py:meth:`fifoio.SharedBuffer.indicate_eof`"""
 
     def test_indicate_eof_flips_flag(self):
         """test if :py:meth:`fifoio.SharedBuffer.indicate_eof` flips the eof flag"""
@@ -34,10 +39,6 @@ class IndicateEofTestCase(TestCase):
                 else:
                     waiting.append("timeout")
 
-        async def run_in_thread(func: Callable, *args) -> None:
-            loop = asyncio.get_running_loop()
-            await loop.run_in_executor(None, func, *args)
-
         async def main() -> None:
             task1 = asyncio.create_task(run_in_thread(wait_for_notify, waiting))
             task2 = asyncio.create_task(run_in_thread(buffer.inidicate_eof))
@@ -48,7 +49,7 @@ class IndicateEofTestCase(TestCase):
 
 
 class BytesFreeTestCase(TestCase):
-    """Tests for :py:class:`fifoio.SharedBuffer.bytes_free`"""
+    """Tests for :py:meth:`fifoio.SharedBuffer.bytes_free`"""
 
     def test_buffer_fresh(self):
         """tests that we have the full buffer available for a fresh object"""
@@ -60,20 +61,126 @@ class BytesFreeTestCase(TestCase):
         """tests that we have the full buffer - no bytes written to it"""
 
         buffer = SharedBuffer(buffer_size=100)
-        buffer.write(b'1' * 10)
+        buffer.write(b"1" * 10)
         self.assertEqual(buffer.bytes_free(), 90)
 
     def test_buffer_full(self):
         """tests that we have the buffer if full after filling it"""
 
         buffer = SharedBuffer(buffer_size=100)
-        buffer.write(b'1' * 100)
+        buffer.write(b"1" * 100)
         self.assertEqual(buffer.bytes_free(), 0)
 
     def test_buffer_read(self):
         """tests that the number of free bytes increased after reading"""
         buffer = SharedBuffer(buffer_size=100)
-        buffer.write(b'1' * 100)
+        buffer.write(b"1" * 100)
         buffer.readinto(bytearray(10))
         self.assertEqual(buffer.bytes_free(), 10)
+
+
+class AvailabilityTestCase(TestCase):
+    """Tests for :py:meth:`fifoio.SharedBuffer.available`"""
+
+    def test_initial_availability(self):
+        """test that initially we have no bytes ready for reading"""
+        buffer = SharedBuffer(buffer_size=100)
+        self.assertEqual(buffer.available(), 0)
+
+    def test_after_write(self):
+        """after writeing we should have more bytes to read"""
+        buffer = SharedBuffer(buffer_size=100)
+        buffer.write(b"0" * 60)
+        self.assertEqual(buffer.available(), 60)
+        buffer.write(b"0" * 10)
+        self.assertEqual(buffer.available(), 70)
+
+    def test_after_read(self):
+        """after a read there should be fewer bytes to"""
+        buffer = SharedBuffer(buffer_size=100)
+        buffer.write(b"0" * 60)
+        self.assertEqual(buffer.available(), 60)
+        buffer.readinto(bytearray(20))
+        self.assertEqual(buffer.available(), 40)
+
+
+class WriteTestCase(TestCase):
+    """Tests for :py:meth:`fifoio.SharedBuffer.write`"""
+
+    def test_read_write(self):
+        """test that we can read what was written"""
+        buffer = SharedBuffer(buffer_size=100)
+        buffer.write(b"abc")
+        result = bytearray(3)
+        buffer.readinto(result)
+        self.assertEqual(b"abc", result)
+
+    def test_read_write_circular(self):
+        """test subsequent read an writes greater than the buffer size"""
+        buffer = SharedBuffer(buffer_size=15)
+        buffer.write(b"0123456789")
+        buffer.readinto(bytearray(10))
+        buffer.write(b"0123456789")
+        buffer.readinto(bytearray(10))
+        buffer.write(b"0123456789")
+        result = bytearray(10)
+        buffer.readinto(result)
+        self.assertEqual(b"0123456789", result)
+
+    def test_block_on_full(self):
+        """test that it blocks when the buffer is full"""
+        buffer = SharedBuffer(buffer_size=10)
+        buffer.write(b"0123456789")
+        self.assertEqual(buffer.bytes_free(), 0)
+        history = []
+
+        def write_blocked() -> None:
+            """write to full buffer"""
+            history.append("before write")
+            buffer.write(b"abc")
+            history.append("after write")
+
+        def read() -> None:
+            """read from buffer"""
+            history.append("before read")
+            buffer.readinto(bytearray(10))
+            history.append("read done")
+
+        async def main() -> None:
+            write_task = asyncio.create_task(run_in_thread(write_blocked))
+            asyncio.sleep(0.1)
+            read_task = asyncio.create_task(run_in_thread(read))
+            await asyncio.gather(write_task, read_task)
+
+        asyncio.run(main())
+        self.assertListEqual(
+            history,
+            [
+                "before write",
+                "before read",
+                "read done",
+                "after write",
+            ],
+        )
+
+    def test_notify_empty(self):
+        """test that the empty condition is notified after a write"""
+        buffer = SharedBuffer(buffer_size=100)
+        waiting = []
+
+        def wait_for_notify(waiting: list) -> None:
+            """"""
+            with buffer._empty:
+                if buffer._empty.wait(timeout=1):
+                    waiting.append("done")
+                else:
+                    waiting.append("timeout")
+
+        async def main() -> None:
+            task1 = asyncio.create_task(run_in_thread(wait_for_notify, waiting))
+            task2 = asyncio.create_task(run_in_thread(buffer.write, b"abc"))
+            await asyncio.gather(task1, task2)
+
+        asyncio.run(main())
+        self.assertEqual(waiting[0], "done")
 
